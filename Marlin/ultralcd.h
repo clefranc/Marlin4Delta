87bacc459bc26f991d5a37b86282d96596dd2086
Christian Lefrançois <clefranc@diffusionfermont.ca>
2015-09-17 20:28:00 -0400

    Set LCD_UPDATE_INTERVAL
    * Set LCD_UPDATE_INTERVAL to 1000 (10s).

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -32,7 +32,7 @@
   #define LCD_MESSAGEPGM(x) lcd_setstatuspgm(PSTR(x))
   #define LCD_ALERTMESSAGEPGM(x) lcd_setalertstatuspgm(PSTR(x))
 
-  #define LCD_UPDATE_INTERVAL 100
+  #define LCD_UPDATE_INTERVAL 1000
   #define LCD_TIMEOUT_TO_STATUS 15000
 
   #if ENABLED(ULTIPANEL)
