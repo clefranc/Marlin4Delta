dccce5e1b9d76e891173e98e6607a224dc26cd44
clefranc <clefranc at diffusionfermont dot ca>
2015-10-06 02:10:43 -0400

    Travis TEST3

--- Marlin/Default_Version.h
+++ Marlin/Default_Version.h
@@ -11,7 +11,7 @@
 #define SHORT_BUILD_VERSION "1.1.0-RC2"
 #define CUSTOM_SHORT_BUILD_VERSION "1.0.0-RC1"
 #define DETAILED_BUILD_VERSION "1.1.0-RC2 From Archive"
-#define CUSTOM_DETAILED_BUILD_VERSION "1.0.0 Release Candidate 1"
+#define CUSTOM_DETAILED_BUILD_VERSION "TEST"
 #define STRING_DISTRIBUTION_DATE "2015-09-28 12:00"
 #define CUSTOM_STRING_DISTRIBUTION_DATE "2015-10-06 12:00"
 // It might also be appropriate to define a location where additional information can be found
