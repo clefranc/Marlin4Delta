1cf219109ad61fed8ee86a061f1da545062e16a2
clefranc <clefranc at diffusionfermont dot ca>
2015-10-06 01:46:14 -0400

    Fix M115 for Travis
    *Fixed M115 for Travis.

--- Marlin/Default_Version.h
+++ Marlin/Default_Version.h
@@ -8,10 +8,13 @@
 #if true
 #define FIRMWARE_NAME "Marlin"
 #define CUSTOM_FIRMWARE_NAME "Marlin4Delta"
-#define SHORT_BUILD_VERSION "1.0.0-RC1"
-#define DETAILED_BUILD_VERSION "Marlin4Delta v1.0.0 Release Candidate 1"
-#define STRING_DISTRIBUTION_DATE "2015-10-04 12:00"
+#define SHORT_BUILD_VERSION "1.1.0-RC2"
+#define CUSTOM_SHORT_BUILD_VERSION "1.0.0-RC1"
+#define DETAILED_BUILD_VERSION "1.1.0-RC2 From Archive"
+#define CUSTOM_DETAILED_BUILD_VERSION "1.0.0 Release Candidate 1"
+#define STRING_DISTRIBUTION_DATE "2015-09-28 12:00"
+#define CUSTOM_STRING_DISTRIBUTION_DATE "2015-10-06 12:00"
 // It might also be appropriate to define a location where additional information can be found
-#define SOURCE_CODE_URL  "github.com/MarlinFirmware/Marlin"
-#define CUSTOM_SOURCE_CODE_URL  "clefranc.github.io/Marlin4Delta"
+#define SOURCE_CODE_URL  "https://github.com/MarlinFirmware/Marlin"
+#define CUSTOM_SOURCE_CODE_URL  "https://clefranc.github.io/Marlin4Delta"
 #endif
